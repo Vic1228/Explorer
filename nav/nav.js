8d173ef5e8bfdc5a471dfe9f609370f52f1619dc
Vic1228 <a078138419@gmail.com>
2022-01-28 16:41:49 +0800

    NAV UPDATE

--- nav/nav.js
+++ nav/nav.js
@@ -1,10 +1,10 @@
 /*---------------------------------------------------------------*/
 /*----------------------------- nav -----------------------------*/
 /*---------------------------------------------------------------*/
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+var script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
+script.type = "text/javascript";
+document.getElementsByTagName("head")[0].appendChild(script);
 
 //監聽滾動，header固定
 // window.onscroll = function () {
@@ -15,40 +15,37 @@ document.getElementsByTagName('head')[0].appendChild(script);
 //     }
 // }
 
-
 //左側navbar滑出動畫
 var navSlide = () => {
-    var burger = document.querySelector('.nav_burger');
-    var nav = document.querySelector('.nav_links_container');
-    var navlinks = document.querySelectorAll('.nav_links li');
+  var burger = document.querySelector(".nav_burger");
+  var nav = document.querySelector(".nav_links_container");
+  var navlinks = document.querySelectorAll(".nav_links li");
 
-    burger.addEventListener('click', () => {
-        //Toggle Nav
-        nav.classList.toggle('nav_active');
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav_active");
 
-        //Animate Links
-        navlinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-                $('#blur_all').css({'backdrop-filter':'none'});
-                $('#blur_all').fadeOut(150);
-
-            }
-            else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 30 + 0.25}s`;
-                $('#blur_all').css({'backdrop-filter':'blur(20px)'});
-                $('#blur_all').fadeIn(150);
-            }
-        });
-
-
-
-        console.log($);
-
-        //Burger Animation
-        burger.classList.toggle('toggle');
+    //Animate Links
+    navlinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+        $("#blur_all").css({ "backdrop-filter": "none" });
+        $("#blur_all").fadeOut(150);
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 30 + 0.25
+        }s`;
+        $("#blur_all").css({ "backdrop-filter": "blur(20px)" });
+        $("#blur_all").fadeIn(150);
+      }
     });
-}
+
+    console.log($);
+
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
 navSlide();
 
 /*---------------------------------------------------------------*/
