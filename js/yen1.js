e6c6207178e7febc5b9d1876752bbf5e100d0e59
ccyen1995 <k603062684@gmail.com>
2022-02-11 10:53:57 +0800

    yen

--- js/yen1.js
+++ js/yen1.js
@@ -2,7 +2,7 @@ var linkbt = document.getElementById("opener");
 var light = document.getElementById('light');
 var closebt = document.getElementById("closebt");
 
-$("#opener").click(function () {
+$(".opener").click(function () {
     $(".light").show(200);
     // $("body").css({"filter": "blur(2px)"});
 });
