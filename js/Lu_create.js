c012abca2ed288176dd115f69cd583654c24da0a
bobeddie <46520580+bobeddie@users.noreply.github.com>
2022-02-17 15:07:14 +0800

    3/4

--- js/Lu_create.js
+++ js/Lu_create.js
@@ -1,4 +1,14 @@
 /*---------------------------------------------------------------*/
+/*----------------------------- date ---------------------------*/
+/*---------------------------------------------------------------*/
+// let startDate = document.getElementById("startDate").value;
+// let endDate = document.getElementById("endDate").value;
+// let dayCount = document.getElementById("dayCount").value;
+
+// console.log(endDate);
+// console.log(startDate);
+
+/*---------------------------------------------------------------*/
 /*----------------------------- chart ---------------------------*/
 /*---------------------------------------------------------------*/
 const data = {
@@ -84,6 +94,8 @@ const myChart = new Chart(
 let currentTab = 0;
 showTab(currentTab);
 
+// 畫面到頂端
+$("html,body").scrollTop(0);
 // 方法
 function showTab(nowPage) {
     // id是唯一的 classname可以重複 所以才能變成陣列
@@ -108,36 +120,61 @@ function showTab(nowPage) {
         document.getElementById("nextBtn").innerHTML = "下一步";
         document.getElementById("nextBtn").style.display = "inline";
     }
-
-    
+    // TODO: 執行步驟燈方法(目前步驟)
+        // fixStepIndicator(nowPage);
 }
 // 換頁 0/1
 function nextPrev(turn) {
     const tabArray = document.getElementsByClassName("tab");
-    // 當前步驟消失
+    // TODO:沒輸入資訊無法下一頁
+        if (turn == 1 && !validForm()) return false;
+    // 當前步驟隱藏
     tabArray[currentTab].style.display = "none";
     // 目前頁面變數更新
     currentTab += turn;
+    // 畫面到頂端
+    $("html,body").scrollTop(0);
     // 執行目前步驟方法
     showTab(currentTab);
 }
 
-// function validForm() {
-//     let valid = true;
-//     const tabArray = document.getElementsByClassName("tab");
-//     const inputArray = tabArray[currentTab].getElementsByClassName("input")
+function validForm() {
+    // 設定變數
+    let valid = true;
+    const tabArray = document.getElementsByClassName("tab");
+    const inputArray = tabArray[currentTab].getElementsByClassName("form-input")
     
-//     for (let i = 0; i < inputArray.length; i++){
-//         if (inputArray[i].value == "") {
-//             inputArray[i].className += "invalid";
-//             valid = false;
-//         }
-//     }
-//     return valid;
-// }
+    // 檢查input是不是沒有東西
+    for (let i = 0; i < inputArray.length; i++){
+        if (inputArray[i].value == "" || inputArray[i].value == undefined) {
+            // 增加 invalid的樣式
+            inputArray[i].className += " invalid";
+            valid = false;
+        }
+    }
+    return valid;
+}
 
 function submitBtnClick() {
+    let valid = true;
+    const tabArray = document.getElementsByClassName("tab");
+    const inputArray = tabArray[currentTab].getElementsByClassName("form-input")
+    
+    // 檢查input是不是沒有東西
+    for (let i = 0; i < inputArray.length; i++){
+        if (inputArray[i].value == "" || inputArray[i].value == undefined) {
+            // 提醒東西沒有填
+            inputArray[i].className += " invalid";
+            valid = false;
+        } else {
+            // 
+        }
+    }
+    // 
+    console.log("123");
     document.createForm.submit();
+    return valid;
+
 }
 /*---------------------------------------------------------------*/
 /*------------------------- table-control -----------------------*/
@@ -145,33 +182,72 @@ function submitBtnClick() {
 
 // 變數
 
+const itineraryRow = `<tr>
+            <td>
+              <select id="" name=”mySelect” class="border-0 text-center h-100">
+                <option value="" selected disabled>第?天</option>
+                <option value=”Taipei”>第1天</option>
+                <option value=”Taoyuan”>第2天</option>
+                <option value=”Hsinchu”>第3天</option>
+                <option value=”Miaoli”>第4天</option>
+              </select>
+            </td>
+            <td><input type="time" name="" id="" class="text-center border-0 h-100 time-input">
+            </td>
+            <td><input class="h-100 border-0 act-input text-center" type="text" class="border-0"
+                placeholder="請輸入活動名稱">
+                <p onclick="addItineraryRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn">
+                +</p>
+                <p onclick="deleteItineraryRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn delete123"
+                   style="margin-inline: 1rem;">
+                   x</p>
+            </td>
+          </tr>`;
+
+const privateItem = `<tr>
+                <td><input type="text" class="text-center border-0 h-100" placeholder="請輸入物品名稱"></td>
+                <td class="w-25"><input type="number" class="text-center border-0 h-100 w-75" placeholder="請輸入數量">
+                </td>
+                <td>
+                  <input type="text" class="text-center border-0 h-100" placeholder="備註">
+                  <p onclick="addPrivateItemRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn">
+                  +</p>
+                  <p onclick="deletePrivateItemRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn" style="margin-inline: 1rem;">
+                    x</p>
+                </td>
+              </tr>`
+const sharedItem = `<tr>
+                <td><input type="text" class="text-center border-0 h-100" placeholder="請輸入物品名稱"></td>
+                <td><input type="number" name="" id="" class="text-center border-0 h-100 w-50" placeholder="請輸入數量">
+                <p onclick="addSharedItemRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn">
+                +</p>
+                <p onclick="deleteSharedItemRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn" style="margin-inline: 1rem;">
+                  x</p>
+                </td>
+              </tr>`
+
 // 方法
 
-function addTableRow() {
-    const tableRow = `<tr>
-                <td>
-                  <select id="" name=”mySelect” class="border-0 text-center h-100">
-                    <option value="" selected disabled>第?天</option>
-                    <option value=”Taipei”>第1天</option>
-                    <option value=”Taoyuan”>第2天</option>
-                    <option value=”Hsinchu”>第3天</option>
-                    <option value=”Miaoli”>第4天</option>
-                  </select>
-                </td>
-                <td><input type="time" name="" id="" class="text-center border-0 h-100 time-input">
-                </td>
-                <td><input class="h-100 border-0 act-input text-center" type="text" class="border-0"
-                    placeholder="請輸入活動名稱">
-                  <p onclick="deleteTableRow()" class="d-inline-block rounded-circle add-delete-btn table-btn"
-                    style="margin-inline: 1rem;">
-                    x</p>
-                  <p onclick="addTableRow()" class="d-inline-block rounded-circle add-delete-btn table-btn">
-                    +</p>
-                </td>
-              </tr>`;
-    dispatchEvent.innerHTML(tableRow);
+// 增加列
+function addItineraryRow(p) {
+    // $("#ItineraryTable>tbody").append(itineraryRow);
+    $(p).closest("tr").after(itineraryRow);
 }
-function deleteTableRow() {
-    
+function addPrivateItemRow(p) {
+    $(p).closest("tr").after(privateItem);
+}
+function addSharedItemRow(p) {
+    $(p).closest("tr").after(sharedItem);
 }
 
+// 刪除列
+// <p onclick="deleteItineraryRow(this)" 第165行
+function deleteItineraryRow(p) {
+    $(p).closest("tr").remove();
+}
+function deletePrivateItemRow(p) {
+    $(p).closest("tr").remove();
+}
+function deleteSharedItemRow(p) {
+    $(p).closest("tr").remove();
+}
