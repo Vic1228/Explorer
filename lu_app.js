de975c9e34790d38c4ee67d9290572b37d096382
bobeddie <46520580+bobeddie@users.noreply.github.com>
2022-02-18 17:50:58 +0800

    測試用

--- lu_app.js
+++ lu_app.js
@@ -1,3 +1,19 @@
+// 透過express架設伺服器
+var express = require('express');
+var app = express();
+
+// express的模組化路由方法
+const router = express.Router();
+
+// 靜態檔案
+app.use(express.static(__dirname));
+
+// "image"就是資料夾
+app.use(express.static("image"));
+app.use(express.static("css"));
+app.use(express.static("js"));
+
+// 資料庫
 var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
@@ -7,14 +23,14 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-// 測試查詢用 by 政霖
-connection.query("SELECT * from trips", function (err, data, fields) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
-});
-// 測試查詢用
+// 設定主路由/
+app.get('/', function (req, res) {
+    // 印出資訊
+    res.send("express is fun!")
+})
 
 connection.end();
+
+var server = app.listen(5000, function () {
+    console.log('Node server is running..');
+});
