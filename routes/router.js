13c40467898e82700864d38eb5316ea5764bfe28
bqu6204 <bqu6204@gmail.com>
2022-02-18 18:11:41 +0800

    router

--- routes/router.js
+++ routes/router.js
@@ -2,43 +2,43 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/',function(req,res){
-    res.redirect('vic_homepage.ejs')
+    res.render('vic_homepage.ejs')
 })
 
 router.get('/spotInfo',function(req,res){
-    res.redirect('vic_spotInfo.ejs')
+    res.render('vic_spotInfo.ejs')
 })
 
 router.get('/map',function(req,res){
-    res.redirect('hong_map.ejs')
+    res.render('hong_map.ejs')
 })
 
 router.get('/login',function(req,res){
-    res.redirect('hong_login.ejs')
+    res.render('hong_login.ejs')
 })
 
 router.get('/trips',function(req,res){
-    res.redirect('han_trips.ejs')
+    res.render('han_trips.ejs')
 })
 
 router.get('/tripInfo',function(req,res){
-    res.redirect('han_tripInfo.ejs')
+    res.render('han_tripInfo.ejs')
 })
 
 router.get('/trophy',function(req,res){
-    res.redirect('yen_trophy.ejs')
+    res.render('yen_trophy.ejs')
 })
 
 router.get('/profile',function(req,res){
-    res.redirect('yen_profile.ejs')
+    res.render('yen_profile.ejs')
 })
 
 router.get('/createTrip',function(req,res){
-    res.redirect('lu_createTrip.ejs')
+    res.render('lu_createTrip.ejs')
 })
 
 router.get('/tripManage',function(req,res){
-    res.redirect('song_tripManage.ejs')
+    res.render('song_tripManage.ejs')
 })
 
 module.exports = router;
