921aa55e3342fd2003e0f8a570497b9283be56fc
bqu6204 <bqu6204@gmail.com>
2022-02-19 17:26:48 +0800

    popOver

--- Ya_大專題 copy/tripManage.js
+++ Ya_大專題 copy/tripManage.js
@@ -1,89 +1,121 @@
-'use strict';
+$(function(){
+    'use strict';
 
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+    var script = document.createElement('script');
+    script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
 
-//左側navbar滑出動畫
-{
-    var navSlide = () => {
-        var burger = document.querySelector('.nav_burger');
-        var nav = document.querySelector('.nav_links_container');
-        var navlinks = document.querySelectorAll('.nav_links li');
+    //左側navbar滑出動畫
+    {
+        var navSlide = () => {
+            var burger = document.querySelector('.nav_burger');
+            var nav = document.querySelector('.nav_links_container');
+            var navlinks = document.querySelectorAll('.nav_links li');
 
-        burger.addEventListener('click', () => {
-            //Toggle Nav
-            nav.classList.toggle('nav_active');
+            burger.addEventListener('click', () => {
+                //Toggle Nav
+                nav.classList.toggle('nav_active');
 
-            //Animate Links
-            navlinks.forEach((link, index) => {
-                if (link.style.animation) {
-                    link.style.animation = '';
-                    $('#blur_all').css({ 'backdrop-filter': 'none' });
-                    $('#blur_all').fadeOut(150);
-                }
-                else {
-                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 30 + 0.25}s`;
-                    $('#blur_all').css({ 'backdrop-filter': 'blur(20px)' });
-                    $('#blur_all').fadeIn(150);
-                }
+                //Animate Links
+                navlinks.forEach((link, index) => {
+                    if (link.style.animation) {
+                        link.style.animation = '';
+                        $('#blur_all').css({ 'backdrop-filter': 'none' });
+                        $('#blur_all').fadeOut(150);
+                    }
+                    else {
+                        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 30 + 0.25}s`;
+                        $('#blur_all').css({ 'backdrop-filter': 'blur(20px)' });
+                        $('#blur_all').fadeIn(150);
+                    }
+                });
+
+                //Burger Animation
+                burger.classList.toggle('toggle');
             });
+        }
+        navSlide();
+    }
 
-            //Burger Animation
-            burger.classList.toggle('toggle');
+    //聊天室input
+    {
+        $('#uploadImg').on('change', function (e) {
+            // console.log(this.value);
+            if (e.target.files.length > 0) {
+                console.log($('#uploadImg').val())
+
+                $('#fileCount').css({
+                    'width': '100%'
+                });
+
+                $('#chatroomText').css(({
+                    'width': '0%',
+                }));
+
+                function showFileCount() {
+                    $('#fileCount span').text(`已選擇 ${e.target.files.length} 個檔案 `);
+                    $('#fileCount').css({ 'opacity': '1' })
+                    $('#chatroomText').css({ 'opacity': '0' })
+
+                }
+
+                setTimeout(showFileCount, 50);
+            } else {
+
+                $('#fileCount').css({
+                    'width': '0%',
+                    'opacity': '0'
+                });
+                function hideFileCount() {
+                    $('#fileCount span').text(``);
+                    $('#chatroomText').css({ 'opacity': '1', 'width': '100%' })
+                }
+                setTimeout(hideFileCount, 50);
+            }
+        })
+
+        $('#fileCountCancel').on('click', function (e) {
+            $('#uploadImg').val(null);
+
+            $('#fileCount').css({
+                'width': '0%',
+                'opacity': '0'
+            });
+            function hideFileCount() {
+                $('#fileCount span').text(``);
+                $('#chatroomText').css({ 'opacity': '1', 'width': '100%' })
+            }
+            setTimeout(hideFileCount, 250);
+        })
+    }
+
+    // popover
+    {
+        $('.itemPopover').popover({
+            trigger: 'hover',
+            html: true,
+            content: function () {
+                var userName = $(this.closest('tr')).children('td:first-child').text();
+                console.log(userName);
+                return `<h1>${userName}</h1>`;
+            }
+        });
+
+        $('.statPopover').popover({
+            trigger: 'hover',
+            html: true,
+            content: function () {
+                var userName = $(this.closest('tr')).children('td:first-child').text();
+                console.log(userName);
+                return `<h1>${userName}</h1>`;
+            }
         });
     }
-    navSlide();
-}
+})
 
-{
-    $('#uploadImg').on('change', function (e) {
-        // console.log(this.value);
-        if (e.target.files.length > 1) {
-            console.log($('#uploadImg').val())
 
-            $('#fileCount').css({
-                'width': '100%'
-            });
 
-            $('#chatroomText').css(({
-                'width': '0%',
-            }));
 
-            function showFileCount(){
-                $('#fileCount span').text(`已選擇 ${e.target.files.length} 個檔案 `);
-                $('#fileCount').css({'opacity':'1'})
-                $('#chatroomText').css({'opacity':'0'})
-                
-            }
 
-            setTimeout(showFileCount,50);
-        } else {
 
-            $('#fileCount').css({
-                'width': '0%',
-                'opacity':'0'
-            });     
-            function hideFileCount(){
-                $('#fileCount span').text(``);
-                $('#chatroomText').css({'opacity':'1','width':'100%'})
-            }
-            setTimeout(hideFileCount,50);
-        }
-    })
-
-    $('#fileCountCancel').on('click',function(e){
-        $('#uploadImg').val(null);
-
-        $('#fileCount').css({
-            'width': '0%',
-            'opacity':'0'
-        });      
-        function hideFileCount(){
-            $('#fileCount span').text(``);
-            $('#chatroomText').css({'opacity':'1','width':'100%'})
-        }
-        setTimeout(hideFileCount,50);
-    })
-}
