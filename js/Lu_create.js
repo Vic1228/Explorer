2d43b0f92094e86edcb1ccd5fb9808d31e170e60
bobeddie <46520580+bobeddie@users.noreply.github.com>
2022-02-20 16:09:52 +0800

    表單的name分別取名對應資料庫

--- js/Lu_create.js
+++ js/Lu_create.js
@@ -170,7 +170,7 @@ function fixStepIndicator(currentStep) {
 //
 const itineraryRow = `<tr>
                 <td class="form-td">
-                  <select id="" name="" class="border-0 text-center h-100 form-input w-100"
+                  <select name="schedules" id="" class="border-0 text-center h-100 form-input w-100"
                     oninput="this.className = 'border-0 text-center h-100 form-input w-100'">
                     <option value="" selected disabled>第?天</option>
                     <option value="1">第1天</option>
@@ -179,13 +179,14 @@ const itineraryRow = `<tr>
                     <option value="4">第4天</option>
                   </select>
                 </td>
-                <td class="form-td"><input type="time" name="" id=""
+                <td class="form-td"><input name="schedules" type="time" id=""
                     class="text-center border-0 h-100 time-input form-input w-100"
                     oninput="this.className = 'text-center border-0 h-100 time-input form-input w-100'">
                 </td>
-                <td class="form-td"><input class="h-100 w-75 border-0 act-input text-center form-input" type="text"
-                    class="border-0" oninput="this.className = 'h-100 w-75 border-0 act-input text-center form-input'"
-                    placeholder="活動名稱" style="margin-right: 1rem;">
+                <td class="form-td"><input name="schedules" class="h-100 w-75 border-0 act-input text-center form-input"
+                    type="text" class="border-0"
+                    oninput="this.className = 'h-100 w-75 border-0 act-input text-center form-input'" placeholder="活動名稱"
+                    style="margin-right: 1rem;">
                   <p onclick="addItineraryRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn">
                     +</p>
                   <p onclick="deleteItineraryRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn"
@@ -195,9 +196,10 @@ const itineraryRow = `<tr>
               </tr>`;
 
 const privateItem = `<tr>
-                <td class="form-td"><input type="text" class="text-center border-0 h-100 form-input" placeholder="物品名稱"
-                    oninput="this.className = 'text-center border-0 h-100 form-input'"></td>
-                <td class="form-td"><input type="number" class="text-center border-0 w-25 h-100 form-input"
+                <td class="form-td"><input name="privateitems" type="text" class="text-center border-0 h-100 form-input"
+                    placeholder="物品名稱" oninput="this.className = 'text-center border-0 h-100 form-input'"></td>
+                <td class="form-td"><input name="privateitems" type="number"
+                    class="text-center border-0 w-25 h-100 form-input"
                     oninput="this.className = 'text-center border-0 w-25 h-100 form-input'" style="margin-right: 1rem;">
                   <p onclick="addPrivateItemRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn">
                     +</p>
@@ -207,9 +209,9 @@ const privateItem = `<tr>
                 </td>
               </tr>`;
 const sharedItem = `<tr>
-                <td class="form-td"><input type="text" class="text-center border-0 h-100 form-input" placeholder="物品名稱"
-                    oninput="this.className = 'text-center border-0 h-100 form-input'"></td>
-                <td class="form-td"><input type="number" name="" id=""
+                <td class="form-td"><input name="shareditems" type="text" class="text-center border-0 h-100 form-input"
+                    placeholder="物品名稱" oninput="this.className = 'text-center border-0 h-100 form-input'"></td>
+                <td class="form-td"><input name="shareditems" type="number" id=""
                     class="text-center border-0 h-100 form-input w-50"
                     oninput="this.className = 'text-center border-0 h-100 form-input w-50'" style="margin-right: 1rem;">
                   <p onclick="addSharedItemRow(this)" class="d-inline-block rounded-circle add-delete-btn table-btn">
