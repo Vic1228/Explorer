96c5c34ef654d3f48758c3f3afe48cca58e9978d
bobeddie <46520580+bobeddie@users.noreply.github.com>
2022-02-22 14:36:02 +0800

    改回原本設定

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",      //if mac ,須設定為root
-  database: "test",
-  port:'3307'
+  password: "",      //if mac ,須設定為root
+  database: "explorer",
+  port:'3306'
 });
 
 // ============= router ===============
