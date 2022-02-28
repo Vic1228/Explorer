d8a420982d56b230cbee2936c3ef7ad923fe0267
ccyen1995 <k603062684@gmail.com>
2022-03-01 04:01:14 +0800

    yen

--- routes/yen_routes/yen_routes.js
+++ routes/yen_routes/yen_routes.js
@@ -4,8 +4,10 @@ var mysql = require("mysql");
 const session = require('express-session');
 const multer = require('multer');
 const fs = require('fs')
+const path = require('path')
 
 const { CLIENT_CONNECT_WITH_DB } = require("mysql/lib/protocol/constants/client");
+const e = require("express");
 // const myChart = new Chart();
 
 var connection = mysql.createConnection({
@@ -72,7 +74,7 @@ router.get("/trophy", function (req, res) {
                                 console.log(json7)
                                 // var total = Object.assign(one,two);
                                 // console.log('total>>>>' + total)
-                                res.render("yen_trophy.ejs", { json1, json2, json3, json4, json5, json6, json7});
+                                res.render("yen_trophy.ejs", { json1, json2, json3, json4, json5, json6, json7 });
                             })
                         })
                     })
@@ -85,19 +87,42 @@ router.get("/trophy", function (req, res) {
 // 個人資料路由
 
 router.get("/profile", (req, res) => {
-    let sqlone = `SELECT * FROM users where userId=2`;
+    let sqlone = `SELECT * FROM users where userId=3`;
     connection.query(sqlone, (err, result, fields) => {
         if (err) throw err;
-        sqltwo = `SELECT * FROM userstats where userId=1`
+        sqltwo = `SELECT * FROM userstats where userId=3`
         connection.query(sqltwo, (err, result2) => {
             if (err) throw err;
+            // 要怎麼利用主檔名搜尋檔案
+            // let filename = path.basename(result[0].userEmail)
+            // let fileext = path.extname(result[0].userEmail)
+            // console.log(filename)
+            // console.log(fileext)
+            // var fs = require('fs');
+
+            fs.readdir('public/img/yen/photo/', function (err, files) {
+                console.log(files)
+                    // .filter(function (file) { return file.substr(-5) === '.html'; })
+                    // .forEach(function (file) { fs.readFile(file, 'utf-8', function (err, contents) { inspectFile(contents); }); });
+            });
+
+            // function inspectFile(contents) {
+            //     if (contents.indexOf('data-template="home"') != -1) {
+            //         // do something
+            //     }
+            // }
+            // let photopath = 'public/img/yen/photo/';
+            // console.log(fs.readdir(photopath+result[0].userEmail, (err) => {
+            //     console.log(err)
+            // }))
+            // fs.readFile(filename, (err) => {
+            //     console.log(err)
+            // })
+
             let b = result2[0];
-            console.log(b);
             let a = result[0];
-            console.log(a);
             var obj = Object.assign(a, b);
-            // res.render("yen_profile.ejs", a,b);
-            console.log(obj)
+            // console.log(obj)
             res.render("yen_profile.ejs", obj);
         })
     });
@@ -110,14 +135,14 @@ const storage = multer.diskStorage({
         cb(null, 'public/img/yen/photo')
     },
     filename: function (req, file, cb) {
-        let sqlres = `SELECT userEmail FROM users where userId= 1`;
+        let sqlres = `SELECT userEmail FROM users where userId= 2`;
         connection.query(sqlres, (err, result, fields) => {
             if (err) throw err;
             let a = result[0];
             let b = a.userEmail
             let name = file.originalname
             let ext = name.split('.').pop()
-            cb(null, b + '.' + ext)
+            cb(null, b + '.png')
         });
     }
 
@@ -135,7 +160,7 @@ router.post('/rephoto', upload.single('upload'), function (req, res) {
 // --修改個人資料
 router.post("/rename", (req, res) => {
     let name = req.body.name;
-    let sql = `UPDATE users SET userName ='${name}' WHERE userId = 1`;
+    let sql = `UPDATE users SET userName ='${name}' WHERE userId = 2`;
     connection.query(sql, (err, result, fields) => {
         if (err) throw err;
         console.log(result);
@@ -144,7 +169,7 @@ router.post("/rename", (req, res) => {
 });
 router.post("/rephone", (req, res) => {
     let tel = req.body.tel;
-    let sql = `UPDATE users SET userPhone ='${tel}' WHERE userId = 1`;
+    let sql = `UPDATE users SET userPhone ='${tel}' WHERE userId = 2`;
     connection.query(sql, (err, result, fields) => {
         if (err) throw err;
         console.log(result);
@@ -153,7 +178,7 @@ router.post("/rephone", (req, res) => {
 });
 router.post("/remail", (req, res) => {
     let email = req.body.mail;
-    let sql = `UPDATE users SET userEmail ='${email}' WHERE userId = 1`;
+    let sql = `UPDATE users SET userEmail ='${email}' WHERE userId = 2`;
     connection.query(sql, (err, result, fields) => {
         if (err) throw err;
         console.log(result);
@@ -162,7 +187,7 @@ router.post("/remail", (req, res) => {
 });
 router.post("/retext", (req, res) => {
     let text = req.body.text;
-    let sql = `UPDATE users SET userExp ='${text}' WHERE userId = 1`;
+    let sql = `UPDATE users SET userExp ='${text}' WHERE userId = 2`;
     connection.query(sql, (err, result, fields) => {
         if (err) throw err;
         console.log(result);
