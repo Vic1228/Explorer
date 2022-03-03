e0ee191e9a7e97024196152adb606d21b2a3c1af
bqu6204 <bqu6204@gmail.com>
2022-03-03 14:08:51 +0800

    123
    123

--- routes/song_routes/song_tripManage.js
+++ routes/song_routes/song_tripManage.js
@@ -24,14 +24,12 @@ bluebird.promisifyAll(conn);
 
 
 // ---------------------- request ---------------------- 
-song_tripManage_router.get('/',function (req, res) {
+song_tripManage_router.get('/', function (req, res) {
     var userId = 1;
     var data = {
         userName: '',
-        createTripIdList: [],
-        createTripNameList: [],
-        joinTripIdList: [],
-        joinTripNameList: [],
+        createTripList: [],
+        joinTripList: [],
         //以下為trip詳細資料
         tripchatboard: [
             {
@@ -39,51 +37,10 @@ song_tripManage_router.get('/',function (req, res) {
                 chatTime: '',
                 chatMessage: ''
             },
-            {
-                userId: '',
-                chatTime: '',
-                chatMessage: ''
-            }
         ],
-        tripMember: {
-            memberList: [
-                {
-                    name: '',
-                    userId: '',
-                    sharedItem: [
-                        { sharedItemName: '', itemCount: '' },
-                        { sharedItemName: '', itemCount: '' }
-                    ],
-                    stat: [{
-                        leadership: '', teamwork: '', strength: '',
-                        heal: '', survival: '', direction: '',
-                        commentCount: ''
-                    }]
-                },
-            ],
-            applyList: [
-                {
-                    name: '',
-                    userId: '',
-                    sharedItem: [
-                        { sharedItemName: '', itemCount: '' },
-                        { sharedItemName: '', itemCount: '' }
-                    ],
-                    stat: [{
-                        leadership: '', teamwork: '', strength: '',
-                        heal: '', survival: '', direction: '',
-                        commentCount: ''
-                    }]
-                },
-            ]
-        },
+        tripMember: [
+        ],
         sharedItems: [
-            {
-                sharedItemName: '',
-                provideList: [
-                    { userId: '', userName: '', itemCount: '' },
-                ]
-            }
         ],
         privateItems: [
             { privateItemName: '', itemCount: '' },
@@ -96,33 +53,90 @@ song_tripManage_router.get('/',function (req, res) {
                 ]
             }
         ],
-        tripNotes:''
+        tripNotes: ''
     };
 
-
-    
-
-    conn.queryAsync(`SELECT tripId, positionState FROM tripmembers WHERE userId = ${userId} ORDER BY tripId , positionState DESC`)
-        .then(result => {
+    var sql1 = `SELECT TM.positionState, T.tripId, T.tripName 
+                FROM tripmembers AS TM 
+                INNER JOIN trips AS T ON  TM.tripId=T.tripId 
+                WHERE TM.userId = ${userId} 
+                ORDER BY positionState DESC`;
+    conn.queryAsync(sql1)
+        .then(result1 => {
             // function
-            result.forEach( item => {
-                if ( item.positionState == 2 ){
-                    data.createTripIdList.push(item.tripId);
+            result1.forEach(item => {
+                if (item.positionState == 2) {
+                    data.createTripList.push({ tripName: item.tripName, tripId: item.tripId });
                 }
-                else if ( item.positionState == 1 ){
-                    data.joinTripIdList.push(item.tripId);
+                else if (item.positionState == 1) {
+                    data.joinTripList.push({ tripName: item.tripName, tripId: item.tripId });
                 }
             })
-            console.log(data.createTripIdList);
-            console.log(data.joinTripIdList);
-            return conn.queryAsync(`SELECT tripName, positionState FROM tripmembers WHERE userId = ${userId} ORDER BY positionState`);
+
+            console.log(data.createTripList);
+            console.log(data.joinTripList)
+
+            var sql2 = '';
+            if (data.createTripList.length > 0) {
+                sql2 = `SELECT TM.tripId,TM.userId, TM.positionState, U.userName ,US.leadership, US.teamwork, US.strength, US.heal, US.survival, US.direction, US.commentCount
+                        FROM tripmembers AS TM 
+                        INNER JOIN users AS U ON TM.userId=U.userId 
+                        INNER JOIN userstats AS US ON TM.userId=US.userId
+                        WHERE TM.tripId=${data.createTripList[0].tripId}
+                        ORDER BY TM.positionState DESC`;
+            }
+            else if (data.joinTripList.length > 0) {
+                sql2 = `SELECT TM.tripId,TM.userId, TM.positionState, U.userName ,US.leadership, US.teamwork, US.strength, US.heal, US.survival, US.direction, US.commentCount
+                        FROM tripmembers AS TM 
+                        INNER JOIN users AS U ON TM.userId=U.userId 
+                        INNER JOIN userstats AS US ON TM.userId=US.userId
+                        WHERE TM.tripId=${data.joinTripList[0].tripId}
+                        ORDER BY TM.positionState DESC`;
+            }
+            else{
+                return;
+            }
+
+            return conn.queryAsync(sql2);
         })
-        .then(bee => {
+        .then(result2 => {
             // console.log(apple);
-           return res.render('song_tripManage.ejs');;
+            result2.forEach( item =>{
+                data.tripMember.push({
+                    name: item.userName,
+                    userId: item.userId,
+                    positionState: item.positionState,
+                    stat:{
+                        leadership: item.leadership,
+                        teamwork: item.teamwork,
+                        strength: item.strength,
+                        heal: item.heal,
+                        survival: item.survival,
+                        direction: item.direction,
+                        commentCount: item.commentCount
+                    },
+                });
+            })
+
+            if(result2.length == 0){
+                return;
+            }
+
+            var sql3 = `SELECT * FROM shareditems where tripId = ${result2[0].tripId} AND itemCount IS NOT NULL ORDER BY sharedItem`
+            return conn.queryAsync(sql3);
+
         })
-        .catch( err => console.log(err) );
-       
+        .then(result3 => {
+            console.log(result3);
+            result3.forEach(item => {
+                data.sharedItems.push({
+                    
+                })
+            })
+            return res.render('song_tripManage.ejs');
+        })
+        .catch(err => console.log(err));
+
 })
 
 song_tripManage_router.post('/', function (req, res) {
