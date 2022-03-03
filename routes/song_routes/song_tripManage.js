9cc74dab5e9e5b7ef8dc4913c7644ef43b3180ed
bqu6204 <bqu6204@gmail.com>
2022-03-03 15:45:56 +0800

    SONG
    SONG

--- routes/song_routes/song_tripManage.js
+++ routes/song_routes/song_tripManage.js
@@ -8,14 +8,7 @@ song_tripManage_router.use(bodyParser.json());
 song_tripManage_router.use(bodyParser.urlencoded({ extended: false }));
 
 // ---------------------- mysql ---------------------- 
-var mysql = require('mysql');
-var conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root", //if mac ,須設定為root
-    database: "explorer",
-    multipleStatements: true
-});
+var conn = require('../db.js')
 
 // ---------------------- bluebird ---------------------- 
 var bluebird = require('bluebird');
@@ -43,7 +36,6 @@ song_tripManage_router.get('/', function (req, res) {
         sharedItems: [
         ],
         privateItems: [
-            { privateItemName: '', itemCount: '' },
         ],
         schedule: [
             {
@@ -93,7 +85,7 @@ song_tripManage_router.get('/', function (req, res) {
                         WHERE TM.tripId=${data.joinTripList[0].tripId}
                         ORDER BY TM.positionState DESC`;
             }
-            else{
+            else {
                 return;
             }
 
@@ -101,12 +93,12 @@ song_tripManage_router.get('/', function (req, res) {
         })
         .then(result2 => {
             // console.log(apple);
-            result2.forEach( item =>{
+            result2.forEach(item => {
                 data.tripMember.push({
                     name: item.userName,
                     userId: item.userId,
                     positionState: item.positionState,
-                    stat:{
+                    stat: {
                         leadership: item.leadership,
                         teamwork: item.teamwork,
                         strength: item.strength,
@@ -118,21 +110,48 @@ song_tripManage_router.get('/', function (req, res) {
                 });
             })
 
-            if(result2.length == 0){
+            if (result2.length == 0) {
                 return;
             }
 
-            var sql3 = `SELECT * FROM shareditems where tripId = ${result2[0].tripId} AND itemCount IS NOT NULL ORDER BY sharedItem`
+            var sql3 = `SELECT * FROM shareditems where tripId = ${result2[0].tripId} AND itemCount IS NOT NULL ORDER BY sharedItem`;
+            // var sql3 = `SELECT DISTINCT sharedItem FROM shareditems where tripId = ${result2[0].tripId} AND itemCount IS NOT NULL ORDER BY sharedItem`;
             return conn.queryAsync(sql3);
 
         })
         .then(result3 => {
             console.log(result3);
-            result3.forEach(item => {
-                data.sharedItems.push({
-                    
-                })
-            })
+            if (result3.length > 0) {
+                data.sharedItems.push(
+                    {
+                        sharedItem: result3[0].sharedItem,
+                        itemCount: [{ userId: result3[0].userId, itemCount: result3[0].itemCount }]
+                    }
+                )
+                for (let i = 1; i < result3.length; i++) {
+                    if (result3[i].sharedItem != result3[i - 1].sharedItem) {
+                        data.sharedItems.push({
+                                sharedItem: result3[i].sharedItem,
+                                itemCount: [{ userId: result3[i].userId, itemCount: result3[i].itemCount }]
+                            })
+                    }
+                    else {
+                        data.sharedItems[data.sharedItems.length-1].itemCount.push(
+                            {userId: result3[i].userId, itemCount: result3[i].itemCount}
+                        )
+                    }
+                }
+            } else {
+                return;
+            }
+            console.log(data);
+
+            var sql4 = `SELECT * FROM privateitems WHERE tripId = ${result3[0].tripId}`;
+            return conn.queryAsync(sql4);
+        })
+        .then( result4 => {
+            console.log(result4);
+
             return res.render('song_tripManage.ejs');
         })
         .catch(err => console.log(err));
