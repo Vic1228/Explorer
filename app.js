98fa8a1944bd602db0eb31e303c8c4bb61c88783
Vic1228 <95247372+Vic1228@users.noreply.github.com>
2022-03-05 18:40:51 +0800

    ASDAS

--- app.js
+++ app.js
@@ -185,4 +185,4 @@ app.use("/tripinfo", tripsinfoRouter);
 app.use("/tripSignup", tripSignup);
 app.use("/", yenpage);
 app.use("/", createTrip);
-app.use("/tripManage", tripManage);
+app.use("/tripManage", tripManage); 
