a891c4c0b78610893ac998729d30ca975cb87f04
bobeddie <46520580+bobeddie@users.noreply.github.com>
2022-03-10 23:31:33 +0800

    引用bluebird

--- routes/lu_routes/lu_createTrip.js
+++ routes/lu_routes/lu_createTrip.js
@@ -26,24 +26,33 @@ lu_createTrip_router.get("/createTrip", function (req, res) {
   if (req.session.userId == undefined) {
     res.redirect("/login");
   } else {
-    
-    //  user判斷
-    let test1 = `SELECT * FROM users WHERE userId = ${req.session.userId}`;
-    connection.query(test1, (err, results, fields) => {
-      if (err) throw err;
-      userProfile = results[0];
-      console.log("userProfile",userProfile, typeof(userProfile));
+    // //  user判斷
+    connection.queryAsync(`SELECT * FROM users WHERE userId = ${req.session.userId}`)
+      .then((results1) => {
+        userProfile = results1[0];
+        //   // 查userstats資料
+        let sql1 = `SELECT * FROM userstats WHERE userId = ${req.session.userId}`;
+        return connection.queryAsync(sql1);
+      })
+      .then((results2) => {
+        userStats = results2[0];
+        let obj = Object.assign(userStats, userProfile, location, login);
+        return res.render("lu_createTrip", obj);
+      })
+      .catch((err) => console.log(err));
 
-    // 查userstats資料
-    let test2 = `SELECT * FROM userstats WHERE userId = ${req.session.userId}`;
-    connection.query(test2, (err, results2, fields) => {
-      if (err) throw err;
-      userStats = results2[0];
-      var obj = Object.assign(userStats, userProfile, location, login);
-      //  TODO: 判斷有沒有大於3
-      res.render("lu_createTrip", obj);
-    });
-  });
+    // let test1 = `SELECT * FROM users WHERE userId = ${req.session.userId}`;
+    // connection.query(test1, (err, results, fields) => {
+    //   userProfile = results[0];
+
+    //   let test2 = `SELECT * FROM userstats WHERE userId = ${req.session.userId}`;
+    //   connection.query(test2, (err, results2, fields) => {
+    //     userStats = results2[0];
+    //     var obj = Object.assign(userStats, userProfile, location, login);
+    //     res.render("lu_createTrip", obj);
+
+    //   });
+    // });
   }
 });
 
@@ -58,70 +67,121 @@ lu_createTrip_router.post("/response", function (req, res) {
   if (req.session.userId == undefined) {
     res.redirect("/login");
   } else {
-  // input同name的 分別存入變數
-  let userId = req.session.userId;
-  let trip = req.body.trip;
-  let schedule = req.body.schedule;
-  let private = req.body.private;
+    // input同name的 分別存入變數
+    let userId = req.session.userId;
+    let trip = req.body.trip;
+    let schedule = req.body.schedule;
+    let private = req.body.private;
     let shared = req.body.shared;
     let selectSpotId = req.body.spotid;
+    let tripId;
 
-  //  trip
-  var tripId;
-  let tripSQL = `INSERT INTO trips (tripName, spotId, tripStartDate, tripEndDate, tripDesc) 
-  VALUES ("${trip[0]}", "${spotId||selectSpotId}", "${trip[2]}", "${trip[3]}", "${trip[1]}")`;
-  
-  connection.query(tripSQL, (err, result, fields) => {
-    if (err) throw err;
-    
-    // 查詢trip的tripId存入變數
-    connection.query(
-      "SELECT * FROM trips",
-      function (error, results) {
-        // 得到最後一筆的tripId
-        tripId = results[results.length - 1].tripId;
-
-        // schedule
+    // ===================================
+    // 傳入 行程簡介
+    connection.queryAsync(`INSERT INTO trips (tripName, spotId, tripStartDate, tripEndDate, tripDesc) 
+    VALUES ("${trip[0]}", "${spotId || selectSpotId}", "${trip[2]}", "${trip[3]}", "${trip[1]}")`)
+      // 找到最新的tripid
+      .then(() => {
+        let tripSQL = "SELECT * FROM trips";
+        return connection.queryAsync(tripSQL);
+      })
+      // 傳入 行程成員
+      .then((results1) => {
+        tripId = results1[results1.length - 1].tripId;
+        let memberSQL = `INSERT INTO tripmembers (tripId, userId, positionState) 
+        VALUES ("${tripId}", "${userId}", 2)`;
+        return connection.queryAsync(memberSQL);
+      })
+      // TODO:
+      // 傳入 行程表
+      .then(() => {
         for (var i = 0; i < schedule.length; i += 3) {
-
           let scheduleSQL = `INSERT INTO schedule (tripId, day, startTime, activity)
           VALUES ("${tripId}", "${schedule[i + 0]}", "${schedule[i + 1]}", "${schedule[i + 2]}")`;
-          connection.query(scheduleSQL, (err, result, fields) => {
-            if (i == schedule.length) {
-            
-            // private
-            for (var j = 0; j < private.length; j += 2) {
-              let privateSQL = `INSERT INTO privateItems (tripId, privateItem, ItemCount) 
-              VALUES ("${tripId}", "${private[j + 0]}", "${private[j + 1]}")`;
-              connection.query(privateSQL, (err, result, fields) => {
-                
-                if (j == private.length) {
-                      // shared
-                      for (var k = 0; k < shared.length; k += 2) {
-                      let sharedSQL = `INSERT INTO sharedItems (tripId, userId, sharedItem, itemCount) 
-                                  VALUES ("${tripId}", "${userId}", "${shared[k + 0]}", "${shared[k + 1]}")`;
-                        connection.query(sharedSQL, (err, result, fields) => {
-
-                          if (k == shared.length) {
-                            // tripmember
-                            let memberSQL = `INSERT INTO tripmembers (tripId, userId, positionState) 
-                                  VALUES ("${tripId}", "${userId}", 2)`;
-                            connection.query(memberSQL, (err, result, fields) => { })
-                          }
-                        });
-                      }    
-                } 
-              });
-            } 
-            }
-          });
+          // console.log(tripId);
+          connection.queryAsync(scheduleSQL);
         }
-      });
-  });
-  // 渲染
-  res.render("lu_createFormComplete.ejs");
-  }
-  
-});
+      })
+      // 傳入 私人裝備
+      .then(() => {
+        for (var i = 0; i < private.length; i += 2) {
+          let privateSQL = `INSERT INTO privateItems (tripId, privateItem, ItemCount) 
+            VALUES ("${tripId}", "${private[i + 0]}", "${private[i + 1]}")`;
+            connection.queryAsync(privateSQL);
+          }
+        })
+        // 傳入 公用裝備
+        .then(() => {
+          for (var i = 0; i < shared.length; i += 2) {
+            let sharedSQL = `INSERT INTO sharedItems (tripId, userId, sharedItem, itemCount) 
+            VALUES ("${tripId}", "${userId}", "${shared[i + 0]}", "${shared[i + 1]}")`;
+            connection.queryAsync(sharedSQL);
+          }
+        })
+        // 跳轉填寫完成
+        .then(() => {
+          return res.render("lu_createFormComplete.ejs");
+        })
+        .catch((err) => console.log(err));
+      }
+    
+    });
+    
+    module.exports = lu_createTrip_router;
+        
+    // ===================================
 
-module.exports = lu_createTrip_router;
+    //  trip
+    //   var tripId;
+    //   let tripSQL = `INSERT INTO trips (tripName, spotId, tripStartDate, tripEndDate, tripDesc) 
+    // VALUES ("${trip[0]}", "${spotId || selectSpotId}", "${trip[2]}", "${trip[3]}", "${trip[1]}")`;
+
+    //   connection.query(tripSQL, (err, result, fields) => {
+    //     if (err) throw err;
+
+    //     // 查詢trip的tripId存入變數
+    //     connection.query(
+    //       "SELECT * FROM trips",
+    //       function (error, results) {
+    //         // 得到最後一筆的tripId
+    //         tripId = results[results.length - 1].tripId;
+
+    //         // schedule
+    //         for (var i = 0; i < schedule.length; i += 3) {
+
+    //           let scheduleSQL = `INSERT INTO schedule (tripId, day, startTime, activity)
+    //         VALUES ("${tripId}", "${schedule[i + 0]}", "${schedule[i + 1]}", "${schedule[i + 2]}")`;
+    //           connection.query(scheduleSQL, (err, result, fields) => {
+    //             if (i == schedule.length) {
+
+    //               // private
+    //               for (var j = 0; j < private.length; j += 2) {
+    //                 let privateSQL = `INSERT INTO privateItems (tripId, privateItem, ItemCount) 
+    //             VALUES ("${tripId}", "${private[j + 0]}", "${private[j + 1]}")`;
+    //                 connection.query(privateSQL, (err, result, fields) => {
+
+    //                   if (j == private.length) {
+    //                     // shared
+    //                     for (var k = 0; k < shared.length; k += 2) {
+    //                       let sharedSQL = `INSERT INTO sharedItems (tripId, userId, sharedItem, itemCount) 
+    //                                 VALUES ("${tripId}", "${userId}", "${shared[k + 0]}", "${shared[k + 1]}")`;
+    //                       connection.query(sharedSQL, (err, result, fields) => {
+
+    //                         if (k == shared.length) {
+    //                           // tripmember
+    //                           let memberSQL = `INSERT INTO tripmembers (tripId, userId, positionState) 
+    //                                 VALUES ("${tripId}", "${userId}", 2)`;
+    //                           connection.query(memberSQL, (err, result, fields) => { })
+    //                         }
+    //                       });
+    //                     }
+    //                   }
+    //                 });
+    //               }
+    //             }
+    //           });
+    //         }
+    //       });
+    //   });
+    //   // 渲染
+    //   res.render("lu_createFormComplete.ejs");
